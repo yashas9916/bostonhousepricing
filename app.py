b4cbeb61bafc35099d757a3c497fd800d2e927a1
yashas <yashas.suresh1@gmail.com>
2023-10-13 21:49:50 +0530

    This is the commit for web application

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import pickle
 from flask import Flask,request,jsonify,url_for,render_template
-import numpy 
-import pandas
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 model=pickle.load(open('reg_model.pkl', 'rb'))
-app.route('/')
+scalar=pickle.load(open('scaling.pkl', 'rb'))
+@app.route('/')
 def home():
     return render_template('home.html')
 
@@ -13,7 +14,15 @@ def home():
 def predict_api():
     data=request.json['data']
     print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+if __name__ == '__main__':
+    app.run(debug=True)
 
-    
+
+
 
                       
